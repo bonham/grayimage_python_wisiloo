9ec82ea3acf832de415f2a92390d4ccf7bb3ea8f
Art Furrer <2823246+bonham@users.noreply.github.com>
2023-08-29 23:17:05 +0200

    first final version 0.1

--- boildown.py
+++ boildown.py
@@ -3,14 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
 
-def img2gray(img):
-
-  sliced = img[...,0:3]
-  gray = np.dot(sliced, [0.299, 0.587, 0.114])
-
-  return gray
-
-
 def projectRGB(rgb):
   "takes rgb sequence ( r, g, b) and create projection with boildown function"
   "boildown rule: R2R1R0G1G0B2B1B0 xor R5R4R3G4G3G2B4B3"
@@ -31,36 +23,31 @@ def projectRGB(rgb):
   g2 = bit(g, 2)
   g3 = bit(g, 3)
   g4 = bit(g, 4)
-  g5 = bit(g, 5)
 
   b0 = bit(b, 0)
   b1 = bit(b, 1)
   b2 = bit(b, 2)
   b3 = bit(b, 3)
   b4 = bit(b, 4)
-  b5 = bit(b, 5)
 
   # "R2R1R0G1G0B2B1B0"
   left = (r2 << 7) + (r1 << 6) + (r0 << 5) + (g1 << 4) + (g0 << 3) + (b2 << 2) + (b1 << 1) + b0
-
   # "R5R4R3G4G3G2B4B3"
   right = (r5 << 7) + (r4 << 6) + (r3 << 5) + (g4 << 4) + (g3 << 3) + (g2 << 2) + (b4 << 1)+ b3
-
   return xor(left, right)
 
 def bit(i, n):
   "returns n'th bit of i"
   return (i >> n) & 1
+
   
 if __name__ == "__main__":
 
   imagebin = Image.open(r'img.png')
   image = np.asarray(imagebin)
 
-  projectNpArray = np.vectorize(projectRGB)
+  gray_image = np.apply_along_axis(projectRGB, 2, image)
 
-  gray_image = projectNpArray(image)
-  print(repr(gray_image))
   plt.imshow(gray_image,cmap="gray")  
   plt.show()
 
--- test_boildown.py
+++ test_boildown.py
@@ -1,5 +1,6 @@
-from boildown import projectRGB, bit
+from boildown import projectRGB, bit, arrayProjection
 from operator import xor
+import numpy as np
 
 class TestBit:
   def testOne(self):
@@ -41,4 +42,38 @@ class TestBoilDown:
 
     result = projectRGB(input)
     assert result == expected
+
+class TestArrayProjection:
+
+  def testOne(self):
+
+    left = 0b11111111
+    right = 0b00101101
+    expected1 = xor(left, right)
+
+    R2R1R0G1G0B2B1B0 = 0b11111111
+    R5R4R3G4G3G2B4B3 = 0b11111111
+    expected2 = xor(R2R1R0G1G0B2B1B0, R5R4R3G4G3G2B4B3)
+
+    ar = np.array(
+      [
+        [
+          [0b00001111, 0b00001111, 0b00001111],
+          [0b11111111, 0b11111111, 0b11111111],
+          [0b00001111, 0b00001111, 0b00001111]
+        ],
+        [
+          [0b00001111, 0b00001111, 0b00001111],
+          [0b00001111, 0b00001111, 0b00001111],
+          [0b00001111, 0b00001111, 0b00001111]
+        ]
+      ]
+    )
+
+    result = np.apply_along_axis(projectRGB, 2, ar)
+
+    assert result.shape == (2,3)
+    assert result[0,0] == expected1
+    assert result[0,1] == expected2
+
 
